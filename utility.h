97c3c6c8b7ba0be8e9ddffa7f82998ebbc2e734f
dan <dan@dog.(none)>
2013-10-02 02:44:30 -0700

    Add

--- utility.h
+++ utility.h
@@ -18,17 +18,19 @@ My402List *Q1, *Q2;
 pthread_t PACKET, SERVER, TOKEN;
  
 sigset_t NEW;
-struct sigaction ACT, ACT_TKN;
+struct sigaction ACT, ACT_TKN, ACT_PKT;
 
 struct timeval TIME_AT_Q1, TIME_AT_Q2, TIME_AT_S, SPENT, SERVICE_TIME, PKT_INTV_ARV_TIME;
-double SQ_SERVICE_TIME;
-int NUM_PACKETS ;
+double SQ_SPENT_TIME;
+//int NUM_PACKETS;
+unsigned long long TOTAL_PACKETS, TOTAL_SERVED;
 int SERVER_DIE ;
 
-int DROPPED, DROPPED_PKT, TOTAL, PACKET_DEAD, TOKEN_DEAD;
+int DROPPED, DROPPED_PKT, PACKET_DEAD, TOKEN_DEAD;
+unsigned long long TOTAL;
 
-struct timeval pkt_service_time;
-struct timeval pkt_system_spent_time;
+//struct timeval pkt_service_time;
+//struct timeval pkt_system_spent_time;
 
 pthread_mutex_t m;
 struct timeval START_TIMEVAL;
@@ -68,11 +70,6 @@ unsigned long long toMilliSeconds(struct timeval a);
 
 struct timeval micro_to_timeval(unsigned long long usec);
 
-int check_format(int result,
-                 int line,
-                 unsigned long long inter_time,
-                 unsigned long long token,
-                 unsigned long long service_time);
 
 struct timeval add_timeval(struct timeval a,
                            struct timeval b);
@@ -88,4 +85,19 @@ struct timeval copy_time_val(struct timeval source, struct timeval dest);
 double toSeconds(struct timeval a);
 
 double square(double a);
+unsigned long long usquare(unsigned long long a);
+
+void block_SIGINT();
+
+void print_stats();
+
+int check_num(char *amt);
+int isNum(char *amt);
+
+int check_format(int result,
+                 int line,
+                 char * inter_time,
+                 char *token,             
+                 char *service_time,
+                 char *num);
 
